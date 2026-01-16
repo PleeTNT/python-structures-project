62bc80675b0d056d2777f3e7822fe6464a4c17dd
PleeTNT <prestolee0428@gmail.com>
2026-01-16 09:39:14 -0800

    added some stuff in the main file

---
+++ python-structures-project/src/python_structures_project/barrels-n-backpacks.py
@@ -0,0 +1,15 @@
+'''
+Barrels and Backpacks
+Preston Anderson
+This is intended to be the backend code for the inventory systems,
+however eventually I will make this visual and forego the whole console situation.
+Please excuse the jank! It'll be better I swear!
+'''
+
+
+def main():
+    print("Welcome")
+
+
+if __name__ == "__main__":
+    main()
