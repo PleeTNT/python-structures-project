9f8fcf2f89233fe7471c243b9bd5e8dd9573874b
PleeTNT <prestolee0428@gmail.com>
2026-01-21 09:32:58 -0800

    added dictionary for both the backpack and barrel

--- python-structures-project/src/python_structures_project/barrels-n-backpacks.py
+++ python-structures-project/src/python_structures_project/barrels-n-backpacks.py
@@ -7,9 +7,29 @@ Please excuse the jank! It'll be better I swear!
 '''
 
 
-def main():
-    print("Welcome")
+#have all the values for the backpack slots (i think this works?)
+backpack = [
+    {'bpslot': 1, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 2, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 3, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 4, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 5, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 6, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 7, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 8, 'itemID': 0, 'quantity': 0},
+    {'bpslot': 9, 'itemID': 0, 'quantity': 0},
+]
 
+#have all the values for the barrel slots
+barrel = [
+    {'baslot': 1, 'itemID': 0, 'quantity': 0},
+    {'baslot': 2, 'itemID': 0, 'quantity': 0},
+    {'baslot': 3, 'itemID': 0, 'quantity': 0},
+    {'baslot': 4, 'itemID': 0, 'quantity': 0},
+    {'baslot': 5, 'itemID': 0, 'quantity': 0},
+    {'baslot': 6, 'itemID': 0, 'quantity': 0},
+    {'baslot': 7, 'itemID': 0, 'quantity': 0},
+    {'baslot': 8, 'itemID': 0, 'quantity': 0},
+    {'baslot': 9, 'itemID': 0, 'quantity': 0},
+]
 
-if __name__ == "__main__":
-    main()
