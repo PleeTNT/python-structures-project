48d078c01a52811952df3f98dc1c5c1c89280edd
PleeTNT <prestolee0428@gmail.com>
2026-01-23 09:15:12 -0800

    got some help

--- python-structures-project/src/python_structures_project/barrels-n-backpacks.py
+++ python-structures-project/src/python_structures_project/barrels-n-backpacks.py
@@ -5,33 +5,7 @@ This is intended to be the backend code for the inventory systems,
 however eventually I will make this visual and forego the whole console situation.
 Please excuse the jank! It'll be better I swear!
 '''
-
-
-#have all the values for the backpack slots (i think this works?)
-backpack = [
-    {'bpslot': 1, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 2, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 3, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 4, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 5, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 6, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 7, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 8, 'itemID': 0, 'quantity': 0},
-    {'bpslot': 9, 'itemID': 0, 'quantity': 0},
-]
-
-#have all the values for the barrel slots
-barrel = [
-    {'baslot': 1, 'itemID': 0, 'quantity': 0},
-    {'baslot': 2, 'itemID': 0, 'quantity': 0},
-    {'baslot': 3, 'itemID': 0, 'quantity': 0},
-    {'baslot': 4, 'itemID': 0, 'quantity': 0},
-    {'baslot': 5, 'itemID': 0, 'quantity': 0},
-    {'baslot': 6, 'itemID': 0, 'quantity': 0},
-    {'baslot': 7, 'itemID': 0, 'quantity': 0},
-    {'baslot': 8, 'itemID': 0, 'quantity': 0},
-    {'baslot': 9, 'itemID': 0, 'quantity': 0},
-]
+#(personal note) Python Programming, for the absolute beginner - Michael Dawson, second edition
 
 #dictionary for item ids? i dont know if this is how it works
 items = [
@@ -39,4 +13,23 @@ items = [
     {'ItemID': 2, 'Name': "Health Potion", 'MaxQuantity': 5},
     {'ItemID': 3, 'Name': "Wooden Round Shield", 'MaxQuantity': 1},
     {'ItemID': 4, 'Name': "Gold Coin", 'MaxQuantity': 999},
+    {'ItemID': 5, 'Name': "Leather Armor", 'MaxQuantity': 1}
 ]
+
+def start():
+    backpack_inventory = ()
+    barrel_inventory = ()
+
+    print("Greetings!")
+    print("There is currently", len(backpack_inventory), "items in the backpack.")
+    print("There is currently", len(barrel_inventory), "items in the barrel.")
+
+    print("Let's help get you started, yes?")
+    backpack_inventory = (
+        "Wooden Sword",
+        "Leather Armor",
+        "Gold Coin",
+    )
+    print("I've added", backpack_inventory, "to the backpack.")
+
+start()
