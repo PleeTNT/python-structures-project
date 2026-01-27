d04c497db36bc763eb9658ed9ae581c4eecb8be9
PleeTNT <prestolee0428@gmail.com>
2026-01-27 09:51:53 -0800

    polished the terminal and fixed errors

--- python-structures-project/src/python_structures_project/barrels-n-backpacks.py
+++ python-structures-project/src/python_structures_project/barrels-n-backpacks.py
@@ -31,19 +31,20 @@ def start():
         "Gold Coin",
     ]
     print("I've added", backpack_inventory, "to the backpack.")
+    return backpack_inventory, barrel_inventory
 
 def menu(backpack_inventory, barrel_inventory):
-    print("1: Add an item to the backpack"
-          "2: Remove an item from the backpack"
-          "3: Move an item from the backpack to the barrel"
-          "4: Move an item from the barrel to the backpack"
-          "5: List both inventories"
+    print("1: Add an item to the backpack\n"
+          "2: Remove an item from the backpack\n"
+          "3: Move an item from the backpack to the barrel\n"
+          "4: Move an item from the barrel to the backpack\n"
+          "5: List both inventories\n"
           "6: Exit")
     menu_option = input("What would you like to do? ")
     if menu_option == "1":
-        add_item_backpack(backpack_inventory, barrel_inventory)
+        add_item_backpack(backpack_inventory)
     elif menu_option == "2":
-        remove_item_backpack(backpack_inventory, barrel_inventory)
+        remove_item_backpack(backpack_inventory)
     elif menu_option == "3":
         move_backpack_to_barrel(backpack_inventory, barrel_inventory)
     elif menu_option == "4":
@@ -58,39 +59,39 @@ def menu(backpack_inventory, barrel_inventory):
     return True
 
 def add_item_backpack(backpack_inventory):
-    add_item_id = input("What Item ID would you like to add? ")
-    if add_item_id <= 5:
-        backpack_inventory += items[add_item_id - 1]
-        print("Added", items[add_item_id - 1], "to the backpack.")
+    add_item_id = int(input("What Item ID would you like to add? "))
+    if add_item_id <= 5 and add_item_id > 0:
+        backpack_inventory.append(items[add_item_id - 1]["Name"])
+        print("Added", items[add_item_id - 1]["Name"], "to the backpack.")
     else:
         print("Item ID not recognized.")
 
 
 def remove_item_backpack(backpack_inventory):
-    remove_item_id = input("What Item ID would you like removed? ")
-    if remove_item_id <= 5:
-        backpack_inventory -= items[remove_item_id - 1]
-        print("Removed", items[remove_item_id - 1], "from the backpack.")
+    remove_item_id = int(input("What Item ID would you like removed? "))
+    if remove_item_id <= 5 and remove_item_id > 0:
+        backpack_inventory.remove(items[remove_item_id - 1]["Name"])
+        print("Removed", items[remove_item_id - 1]["Name"], "from the backpack.")
         return backpack_inventory
     else:
         print("Item ID not recognized.")
 
 def move_backpack_to_barrel(backpack_inventory, barrel_inventory):
-    move_item_id = input("What Item ID would you like to move to the barrel? ")
-    if move_item_id <= 5:
-        backpack_inventory -= items[move_item_id - 1]
-        barrel_inventory += items[move_item_id - 1]
-        print("Moved", items[move_item_id - 1], "to the barrel.")
+    move_item_id = int(input("What Item ID would you like to move to the barrel? "))
+    if move_item_id <= 5 and move_item_id > 0:
+        backpack_inventory.remove(items[move_item_id - 1]["Name"])
+        barrel_inventory.append(items[move_item_id - 1]["Name"])
+        print("Moved", items[move_item_id - 1]["Name"], "to the barrel.")
         return backpack_inventory, barrel_inventory
     else:
         print("Item ID not recognized.")
 
 def move_barrel_to_backpack(backpack_inventory, barrel_inventory):
-    move_item_id = input("What Item ID would you like to move to the backpack? ")
-    if move_item_id <= 5:
-        barrel_inventory -= items[move_item_id - 1]
-        backpack_inventory += items[move_item_id - 1]
-        print("Moved", items[move_item_id - 1], "to the backpack.")
+    move_item_id = int(input("What Item ID would you like to move to the backpack? "))
+    if move_item_id <= 5 and move_item_id > 0:
+        barrel_inventory.remove(items[move_item_id - 1]["Name"])
+        backpack_inventory.append(items[move_item_id - 1]["Name"])
+        print("Moved", items[move_item_id - 1]["Name"], "to the backpack.")
     else:
         print("Item ID not recognized.")
 
