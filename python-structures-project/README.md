ce4f85108d30ce7e50b4c711496943792ec075b5
PleeTNT <prestolee0428@gmail.com>
2026-03-03 09:50:32 -0800

    added a menu to display item ids
    i also fixed the errors, and added a way to add and remove multiple items at once

--- python-structures-project/README.md
+++ python-structures-project/README.md
@@ -1,24 +1,10 @@
 Barrels and Backpacks
-by Preston Anderson
-This is intended to be the backend code for the inventory systems, however eventually I will 
-make this visual and forego the whole console situation. 
-Please excuse the jank! It'll be better I swear!
-Initially I had planned for the whole loop of the project to be different by going through 
-different steps of adding, removing, and moving items through each inventory,
-but I found it easier (and through explanation by you, mr winnika) to make it a menu for your options instead.
+By Preston Anderson
+This is intended to be the backend code for the inventory systems, however eventually I will make this visual and forego the whole console situation.
+Initially I had planned for the whole loop of the project to be different by going through different steps of adding, removing, and moving items through each inventory,
+but I found it easier to make it a menu for your options instead.
 
-For the ItemIDs, they are as follows:
+This project is now a project for fun, as this will no longer be continued. The visual version of this project will come some day.
+It works perfectly as is, and should be able to be edited to add anything further if necessary without breaking the code.
 
-ItemID 1 Name = "Wooden Sword"
-ItemID 2 Name = "Health Potion"
-ItemID 3 Name = "Wooden Round Shield"
-ItemID 4 Name = "Gold Coin"
-ItemID 5 Name = "Leather Armor"
-
-Two main errors are if you don't have the required amount of items in the barrel or backpack
-when you are moving it between inventories, it breaks the entire code,
-and also you can only add 1 item at a time with no cap to the items currently.
-This is what I plan to fix next opportunity I get, however in the state that the project is in now,
-I believe that it is perfect as is for a "rough draft" of sorts.
-
-Thank you for your assistance and for the fun project, I look forward to finishing this next semester :]
+Thank you for using this janky inventory.
--- python-structures-project/src/python_structures_project/barrels-n-backpacks.py
+++ python-structures-project/src/python_structures_project/barrels-n-backpacks.py
@@ -7,9 +7,11 @@ items = [
     {'ItemID': 2, 'Name': "Health Potion", 'MaxQuantity': 5},
     {'ItemID': 3, 'Name': "Wooden Round Shield", 'MaxQuantity': 1},
     {'ItemID': 4, 'Name': "Gold Coin", 'MaxQuantity': 999},
-    {'ItemID': 5, 'Name': "Leather Armor", 'MaxQuantity': 1}
+    {'ItemID': 5, 'Name': "Leather Armor", 'MaxQuantity': 1},
+    {'ItemID': 6, 'Name': "Stone Sword", 'Max Quantity': 1}
 ]
 
+#Start dialogue, gives you items to start off with and moves to the menu
 def start():
     barrel_inventory = []
     backpack_inventory = []  
@@ -27,13 +29,21 @@ def start():
     print("I've added", backpack_inventory, "to the backpack.")
     return backpack_inventory, barrel_inventory
 
+#The menu, pretty self explanitory here
 def menu(backpack_inventory, barrel_inventory):
-    print("1: Add an item to the backpack\n"
+    print("\n"
+          "\n"
+          "\n"
+          "1: Add an item to the backpack\n"
           "2: Remove an item from the backpack\n"
           "3: Move an item from the backpack to the barrel\n"
           "4: Move an item from the barrel to the backpack\n"
           "5: List both inventories\n"
-          "6: Exit")
+          "6: Show Item IDs\n"
+          "7: Exit"
+          "\n"
+          "\n"
+          "\n")
     menu_option = input("What would you like to do? ")
     if menu_option == "1":
         add_item_backpack(backpack_inventory)
@@ -46,53 +56,122 @@ def menu(backpack_inventory, barrel_inventory):
     elif menu_option == "5":
         list_inventories(backpack_inventory, barrel_inventory)
     elif menu_option == "6":
+        list_item_ids(items)
+    elif menu_option == "7":
         print("Goodbye!")
         return False
     else:
         print("Invalid option. Please try again.")
     return True
 
+#Ask for the item the user would want to be added, and also how many will be added
 def add_item_backpack(backpack_inventory):
-    add_item_id = int(input("What Item ID would you like to add? "))
-    if add_item_id <= 5 and add_item_id > 0:
-        backpack_inventory.append(items[add_item_id - 1]["Name"])
-        print("Added", items[add_item_id - 1]["Name"], "to the backpack.")
-    else:
+    try:
+        add_item_id = int(input("What Item ID would you like to add? "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+    if not (1 <= add_item_id <= len(items)):
         print("Item ID not recognized.")
+        return
+    try:
+        quantity = int(input("How many would you like to add? "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+    if quantity <= 0:
+        print("Quantity must be greater than 0.")
+        return
+    item = items[add_item_id - 1]
+    name = item["Name"]
+    max_qty = item.get("MaxQuantity")
+    current_count = backpack_inventory.count(name)
+    if current_count + quantity > max_qty:
+        print(f"Cannot add {quantity} {name}: would exceed max quantity ({max_qty}). Current: {current_count}")
+        return
+    for _ in range(quantity):
+        backpack_inventory.append(name)
+    print(f"Added {quantity} {name} to the backpack.")
+    return backpack_inventory
 
-
+#Remove items
 def remove_item_backpack(backpack_inventory):
-    remove_item_id = int(input("What Item ID would you like removed? "))
-    if remove_item_id <= 5 and remove_item_id > 0:
-        backpack_inventory.remove(items[remove_item_id - 1]["Name"])
-        print("Removed", items[remove_item_id - 1]["Name"], "from the backpack.")
-        return backpack_inventory
-    else:
+    try:
+        remove_item_id = int(input("What Item ID would you like removed? "))
+    except ValueError:
+        print("That's not possible, try again.")
+        return
+    if not (1 <= remove_item_id <= len(items)):
         print("Item ID not recognized.")
+        return
+    try:
+        quantity = int(input("How many would you like to remove? "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+    if quantity <= 0:
+        print("Quantity must be greater than 0.")
+        return
+    name = items[remove_item_id - 1]["Name"]
+    current_count = backpack_inventory.count(name)
+    if current_count == 0:
+        print(f"Cannot remove {name}: not in the backpack.")
+        return
+    if quantity > current_count:
+        print(f"Cannot remove {quantity} {name}: only have {current_count}.")
+        return
+    for _ in range(quantity):
+        backpack_inventory.remove(name)
+    print(f"Removed {quantity} {name} from the backpack.")
+    return backpack_inventory
 
+#Move items from the backpack to the barrel, can crash if you are trying to move a ghost item
 def move_backpack_to_barrel(backpack_inventory, barrel_inventory):
-    move_item_id = int(input("What Item ID would you like to move to the barrel? "))
-    if move_item_id <= 5 and move_item_id > 0:
-        backpack_inventory.remove(items[move_item_id - 1]["Name"])
-        barrel_inventory.append(items[move_item_id - 1]["Name"])
-        print("Moved", items[move_item_id - 1]["Name"], "to the barrel.")
-        return backpack_inventory, barrel_inventory
-    else:
+    try:
+        move_item_id = int(input("What Item ID would you like to move to the barrel? "))
+    except ValueError:
+        print("That isn't possible, please try again.")
+        return
+    if not (1 <= move_item_id <= len(items)):
         print("Item ID not recognized.")
+        return
+    name = items[move_item_id - 1]["Name"]
+    if name not in backpack_inventory:
+        print(f"Cannot move {name}: not in the backpack.")
+        return
+    backpack_inventory.remove(name)
+    barrel_inventory.append(name)
+    print("Moved", name, "to the barrel.")
+    return backpack_inventory, barrel_inventory
 
+#Move items from the barrel to the backpack, can crash if you are trying to move a ghost item
 def move_barrel_to_backpack(backpack_inventory, barrel_inventory):
-    move_item_id = int(input("What Item ID would you like to move to the backpack? "))
-    if move_item_id <= 5 and move_item_id > 0:
-        barrel_inventory.remove(items[move_item_id - 1]["Name"])
-        backpack_inventory.append(items[move_item_id - 1]["Name"])
-        print("Moved", items[move_item_id - 1]["Name"], "to the backpack.")
-    else:
+    try:
+        move_item_id = int(input("What Item ID would you like to move to the backpack? "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+    if not (1 <= move_item_id <= len(items)):
         print("Item ID not recognized.")
+        return
+    name = items[move_item_id - 1]["Name"]
+    if name not in barrel_inventory:
+        print(f"Cannot move {name}: not in the barrel.")
+        return
+    barrel_inventory.remove(name)
+    backpack_inventory.append(name)
+    print("Moved", name, "to the backpack.")
 
+#Shows inventories of the barrel and backpack
 def list_inventories(backpack_inventory, barrel_inventory):
     print("Backpack Inventory:", backpack_inventory)
     print("Barrel Inventory:", barrel_inventory)
 
+def list_item_ids(items):
+    for item in items:
+        print(f"{item['ItemID']}: {item['Name']}")
+
+#Pulls all of the tuples down and runs the start code, then loops everything to the menu
 if __name__ == "__main__":
     backpack_inventory, barrel_inventory = start()
     repeat = True
